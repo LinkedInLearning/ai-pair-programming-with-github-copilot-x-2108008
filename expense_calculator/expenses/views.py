4151d14ad7ad5936fee4e2fdf83f7de32a370ee6
Ronnie Sheer <ronysheer@gmail.com>
2025-02-17 14:54:39 +0200

    remove serializer and viewset

--- expense_calculator/expenses/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-
-from .serializers import ExpenseSerializer
-from .models import Expense
-
-class ExpenseViewSet(ModelViewSet):
-    queryset = Expense.objects.all()
-    serializer_class = ExpenseSerializer
